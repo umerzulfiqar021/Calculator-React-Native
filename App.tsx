e6189cc798313dfa9afbadf83c5ea991b3910fa4
umerzulfiqar021 <umerzulfiqar38@gmail.com>
2024-10-18 20:43:34 +0500

    checking

--- App.tsx
+++ App.tsx
@@ -1,105 +1,14 @@
-import React, { useState } from 'react';
-import { View, TextInput, StyleSheet, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-const App = () => {
-  const [input, setInput] = useState('');
-  const [result, setResult] = useState('');
-
-  const onButtonPress = (value) => {
-    
-    if (value === '=') {
-      try {
-        // if (input !== '') {
-          setResult(eval(input));
-        // }
-      } catch (error) {
-        setResult('error');
-      }
-    } else if (value === 'C') {
-      setInput('');
-      setResult('');
-    } else {
-      setInput(input + value);
-    }
-  };
-
+const yup = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.resultContainer}>
-        <Text style={styles.resultText}>{result}</Text>
-      </View>
-      <View style={styles.inputContainer}>
-        <TextInput
-          value={input}
-          onChangeText={setInput}
-          keyboardType='numeric'
-          style={styles.inputText}
-          placeholder="0"
-        />
-      </View>
-      <View style={styles.buttonsContainer}>
-        {['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', 'C', '=', '+'].map((item, index) => (
-          <TouchableOpacity
-            style={styles.button}
-            key={index}
-            onPress={() => onButtonPress(item)}
-          >
-            <Text style={styles.buttonText}>{item}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-    </SafeAreaView>
-  );
-};
+    <View>
+      <Text>yup</Text>
+    </View>
+  )
+}
 
-export default App;
+export default yup
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f0f0f0',
-    justifyContent: 'center',
-    padding: 10,
-  },
-  resultContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'flex-end',
-    backgroundColor: '#fff',
-    padding: 10,
-    borderRadius: 10,
-  },
-  resultText: {
-    fontSize: 36,
-    fontWeight: 'bold',
-  },
-  inputContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-    padding: 10,
-    borderRadius: 10,
-    marginVertical: 10,
-  },
-  inputText: {
-    fontSize: 28,
-    color: '#000',
-  },
-  buttonsContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-  button: {
-    backgroundColor: '#007BFF',
-    padding: 20,
-    width: '22%',
-    margin: '1%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 10,
-  },
-  buttonText: {
-    fontSize: 24,
-    color: '#fff',
-  },
-});
+const styles = StyleSheet.create({})
